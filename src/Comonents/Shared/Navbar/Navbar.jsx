e7fb16a36f9a64aac7cac1d142a06dc91db7d59e
habibur-01 <h.r.habibur08@gmail.com>
2024-03-31 22:16:06 +0600

    set Routes

---
+++ src/Comonents/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import profile from "../../../assets/profile.png"
+
+const Navbar = () => {
+    return (
+        <div>
+            <div>
+                <div className="w-20 h-20">
+                    <img src={profile} alt="" />
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Navbar;
